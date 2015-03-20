75bf9f529e34ab78282ade847a6dcaf38f6c9ef5
normanfong <normanfong@workfront.com>
2015-03-20 17:15:43 -0600

    new rpm

--- workfront-usernotebuilder-service/src/test/java/com/workfront/usernotebuilder/filter/AbstractFilterTest.java
+++
@@ -1,8 +0,0 @@
-package com.workfront.usernotebuilder.filter;
-
-import com.workfront.usernotebuilder.*;
-
-public abstract class AbstractFilterTest extends AbstractUnitTest {
-	public abstract void predicate() throws Exception;
-	public abstract void chain() throws Exception;
-}
